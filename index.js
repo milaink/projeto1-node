9d5d5a5ca29b29cdbf1f75fe4ec4c5e1a9fdc1c2
camila de medeiros <62712691+milaink@users.noreply.github.com>
2021-04-27 22:08:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express"); //importa o módulo express neste arquivo
+const app = express(); //iniciando o express
+
+//criando a rota inicial
+app.get("/", function(req, res) {
+    res.send("<h1>Bem vindo ao meu site!</h1>");
+})
+
+//rota do cadastro de produtos
+app.get("/produtos", function(req, res) {
+    res.send("<h1>Lista de Produtos!</h1>");
+})
+
+//rota com parametro 
+
+app.listen(4000, function(erro) { // cria a aplicação na porta 4000
+    if (erro) {
+        console.log("Erro ao Iniciar.");
+    } else {
+        console.log("Servidor Iniciado.");
+    }
+})
